22098e01a2ec29fc69c8fea277e4dbea93c37a01
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-03-23 19:22:18 +0800

    Minor changes regarding defiitions.     Added Ball typedef.     Renamed array from BALL to BALL_MASK.     Fixed typo in array declaration.

--- Ball.h
+++ Ball.h
@@ -4,7 +4,16 @@
 #define NONE 0X00000000
 #define DIAMETER 15
 
-const unsigned int BALL[][DIAMETER] = {
+struct Ball_s{
+    	int x; /*horizontal anchor*/
+    	int y; /*vertical anchor*/
+    	int d; /*direction*/
+    	int s; /*speed*/
+    	int c; /*color*/
+} Ball_default = {0, 0, 0, 50, 0};
+typedef struct Ball_s Ball;
+
+const unsigned int BALL_MASK[][DIAMETER] = {
                                 {NONE,NONE,NONE,NONE,NONE,FILL,FILL,FILL,FILL,FILL,NONE,NONE,NONE,NONE,NONE},
                                 {NONE,NONE,NONE,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,NONE,NONE,NONE},
                                 {NONE,NONE,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,NONE,NONE},
@@ -19,16 +28,14 @@ const unsigned int BALL[][DIAMETER] = {
                                 {NONE,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,NONE},
                                 {NONE,NONE,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,NONE,NONE},
                                 {NONE,NONE,NONE,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,FILL,NONE,NONE,NONE},
-                                {NONE,NONE,NONE,NONE,NONE,FILL,FILL,FILL,FILL,FILL,NONE,NONE,NONE,NONE,NONE},
+                                {NONE,NONE,NONE,NONE,NONE,FILL,FILL,FILL,FILL,FILL,NONE,NONE,NONE,NONE,NONE}
                             };
-
-
 
 int main(void){
     int i, j;
     for(i = 0; i < DIAMETER; i++){
         for(j = 0; j < DIAMETER; j++){
-            if(BALL[i][j]){
+            if(BALL_MASK[i][j]){
                 printf("O");
             }
             else{
