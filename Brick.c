2825605fe38c65387250460278e9cf1a812d8a45
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-03-23 23:53:27 +0800

    Fixed definitions in C file, where they should've been in H file.

--- Brick.c
+++ Brick.c
@@ -1,12 +1,1 @@
-#define BRICK_WIDTH     40
-#define BRICK_HEIGHT    15
-#define BRICK_SPACING   5
-#define BRICK_SIZE      12;
 
-struct Brick_s{
-    const int x; /*horizontal anchor*/
-    const int y; /*vertical anchor*/
-    int c;       /*color*/
-};
-
-typedef struct Brick_s Brick;
--- Brick.h
+++ Brick.h
@@ -6,3 +6,13 @@
 
 #define BRICK_COLOR_DEFAULT 0x0000FF00 //Green
 #define BRICK_COLOR_ACTIVE  0x00FFFF00 //Yellow
+
+#define BRICK_SIZE      12;
+
+struct Brick_s{
+    const int x; /*horizontal anchor*/
+    const int y; /*vertical anchor*/
+    int c;       /*color*/
+};
+
+typedef struct Brick_s Brick;
