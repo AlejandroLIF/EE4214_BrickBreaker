09613681071bf0ff20eddee281caf291fd85249e
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-03-23 13:32:35 +0800

    Format fix: Replaced tabs with spaces.

--- Bar.h
+++ Bar.h
@@ -1,8 +1,8 @@
-#define BAR_WIDTH		80
-#define BAR_HEIGHT		5
-#define A_REGION_WIDTH	10
-#define S_REGION_WIDTH	10
-#define N_REGION_WIDTH	40
+#define BAR_WIDTH       80
+#define BAR_HEIGHT      5
+#define A_REGION_WIDTH  10
+#define S_REGION_WIDTH  10
+#define N_REGION_WIDTH  40
 
-#define JUMP_SPEED	25	//This is the rate of positional change in terms of pixels per jump
-#define MOVE_SPEED	200 //TODO: adjust this metric to pixels per second, depending on the desired framerate
+#define JUMP_SPEED  25  //This is the rate of positional change in terms of pixels per jump
+#define MOVE_SPEED  200 //TODO: adjust this metric to pixels per second, depending on the desired framerate
