b7521496b573f5990fb484510dcb6620b4a56c77
BlackRage83 <alejandrolealislaflores@hotmail.com>
2016-03-28 13:52:22 +0800

    Added function prototypes. Moved bar movement codes to the bar header.

--- Bar.h
+++ Bar.h
@@ -8,9 +8,19 @@
 #define MOVE_SPEED  200 //TODO: adjust this metric to pixels per second, depending on the desired framerate
 #define  BAR_SIZE    8;
 
+typedef enum{
+    BAR_NO_MOVEMENT,
+    BAR_MOVE_LEFT,
+    BAR_MOVE_RIGHT,
+    BAR_JUMP_LEFT,
+    BAR_JUMP_RIGHT
+} BarMovementCode;
+
 struct Bar_s{
     int x; /*horizontal anchor*/
     const int y; /*vertical anchor*/
 } Bar_default = {0, 0};
 
 typedef struct Bar_s Bar;
+
+void updateBar(Bar* bar, BarMovementCode);
