cf14bc3595d629ac83b09e00711776d39a0321b9
Simmz64 <sim.gollbo@gmail.com>
2016-03-29 23:49:14 +0800

    Added region color definitions

--- Bar.h
+++ Bar.h
@@ -4,6 +4,10 @@
 #define S_REGION_WIDTH  10
 #define N_REGION_WIDTH  41
 
+#define A_REGION_COLOR 0x00ff0000
+#define S_REGION_COLOR 0x000000ff
+#define N_REGION_COLOR 0x00000000
+
 #define JUMP_SPEED  25  //This is the rate of positional change in terms of pixels per jump
 #define MOVE_SPEED  200 //TODO: adjust this metric to pixels per second, depending on the desired framerate
 #define BAR_Y		405
