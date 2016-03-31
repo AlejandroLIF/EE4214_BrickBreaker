3c4363747e93118cc7132317bf51e0ff143ebad8
Simmz64 <sim.gollbo@gmail.com>
2016-03-31 17:30:25 +0800

    Added color constants and coordinates for background and scorescreen

--- MainScreen.h
+++ MainScreen.h
@@ -1,10 +1,19 @@
 #ifndef MAINSCREEN_H
 #define MAINSCREEN_H
 
-#define RIGHT_WALL 514
-#define LEFT_WALL 59
-#define FLOOR 420
-#define CEIL 60
+#define RIGHT_WALL 			514
+#define LEFT_WALL 			59
+#define FLOOR 				420
+#define CEIL 				60
+#define SCORE_RIGHT_WALL	524
+#define SCORE_LEFT_WALL		629
+#define SCORE_FLOOR			101
+#define SCORE_CEIL			60
+
+#define BACKGROUND_COLOR    0x00000000
+#define SCOREAREA_COLOR		0x007A7A7A
+#define GAMEAREA_COLOR		0x00138A15
+
 
 #define FALSE   0
 #define TRUE    1
