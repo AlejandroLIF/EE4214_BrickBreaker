1f2463347db07c95ff49b1a694a4f6b8ba75ed26
Simmz64 <sim.gollbo@gmail.com>
2016-04-12 22:48:57 +0800

    Recycled wallCollision

--- primarycore.c
+++ primarycore.c
@@ -371,7 +371,7 @@ void running(void){
     if(wallCollision == COLLIDE_WALL_FLOOR) {
         loseLife = 1;
     } else {
-        updateBallDirection(&ball, checkCollideWall(&ball));
+        updateBallDirection(&ball, wallCollision);
     }
 
     //Check collision with bar
